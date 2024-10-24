4056fa53bcc89aa18c3012c8aedc07f7173803c1
Rogério Brito <rogeriobritoufu@gmail.com>
2024-10-24 10:07:40 -0300

    Prject init

---
+++ src/main/java/com/rogeriofbrito/litmusgraphqljava/LitmusGraphqlJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rogeriofbrito.litmusgraphqljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitmusGraphqlJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitmusGraphqlJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogeriofbrito/litmusgraphqljava/LitmusGraphqlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogeriofbrito.litmusgraphqljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitmusGraphqlJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
