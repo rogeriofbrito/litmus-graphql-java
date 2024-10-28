cf74c155ab5978b8bb3ece4ceb80a3f1d9d1086d
Rogério Brito <rogeriobritoufu@gmail.com>
2024-10-28 14:28:29 -0300

    add projection fields to request

--- src/main/java/com/rogeriofbrito/litmusgraphqljava/MyGraphQL.java
+++ src/main/java/com/rogeriofbrito/litmusgraphqljava/MyGraphQL.java
@@ -1,11 +1,16 @@
 package com.rogeriofbrito.litmusgraphqljava;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import com.netflix.graphql.dgs.client.codegen.BaseProjectionNode;
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
 import com.rogeriofbrito.graphqlmusicstoremaven.generated.client.ListExperimentGraphQLQuery;
 import com.rogeriofbrito.graphqlmusicstoremaven.generated.client.ListExperimentProjectionRoot;
 import com.rogeriofbrito.graphqlmusicstoremaven.generated.types.ListExperimentRequest;
+import com.rogeriofbrito.graphqlmusicstoremaven.generated.types.ListExperimentResponse;
 import com.rogeriofbrito.graphqlmusicstoremaven.generated.types.Pagination;
 import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
 import jakarta.annotation.PostConstruct;
@@ -21,41 +26,117 @@ public class MyGraphQL {
 
     @PostConstruct
     private void postConstruct() throws JsonProcessingException {
-        ListExperimentGraphQLQuery listExperimentGraphQLQuery = ListExperimentGraphQLQuery
-                .newRequest()
-                .projectID("test")
-                .request(ListExperimentRequest
+        BaseProjectionNode projection = new ListExperimentProjectionRoot<>()
+                .totalNoOfExperiments()
+                .experiments()
+                    //.__typename()
+                    .weightages()
+                        //.__typename()
+                        .faultName()
+                        .weightage()
+                        .getParent()
+                    .infra()
+                        //.__typename()
+                        .createdBy()
+                            //.__typename()
+                            .userID()
+                            .username()
+                            .email()
+                            .getParent()
+                        .updatedBy()
+                            //.__typename()
+                            .userID()
+                            .username()
+                            .email()
+                            .getParent()
+                        //.infraType()
+                        //    .__typename()
+                        //    .getParent()
+                        //.updateStatus()
+                        //    .__typename()
+                        //    .getParent()
+                        .projectID()
+                        .infraID()
+                        .name()
+                        .description()
+                        .tags()
+                        .environmentID()
+                        .platformName()
+                        .isActive()
+                        .isInfraConfirmed()
+                        .isRemoved()
+                        .updatedAt()
+                        .createdAt()
+                        .noOfExperiments()
+                        .noOfExperimentRuns()
+                        .token()
+                        .infraNamespace()
+                        .serviceAccount()
+                        .infraScope()
+                        .infraNsExists()
+                        .infraSaExists()
+                        .lastExperimentTimestamp()
+                        .startTime()
+                        .version()
+                        .getParent()
+                    .createdBy()
+                        //.__typename()
+                        .userID()
+                        .username()
+                        .email()
+                        .getParent()
+                    //.recentExperimentRunDetails()
+                        //.__typename()
+                        //.getParent()
+                    .updatedBy()
+                        //.__typename()
+                        .userID()
+                        .username()
+                        .email()
+                        .getParent()
+                    .projectID()
+                    .experimentID()
+                    .experimentManifest()
+                    .cronSyntax()
+                    .name()
+                    .description()
+                    .isCustomExperiment()
+                    .updatedAt()
+                    .createdAt()
+                    .isRemoved()
+                    .tags()
+                    .description();
+
+        Map<String, ?> variables = Map.of(
+                "projectID","ac3598af-0c62-4ad5-a323-848cafaaa881",
+                "request", ListExperimentRequest
                         .newBuilder()
                         .pagination(Pagination
                                 .newBuilder()
                                 .page(0)
                                 .limit(7)
                                 .build())
-                        .build())
-                .build();
-        var root = new ListExperimentProjectionRoot<>().totalNoOfExperiments();
-        var experimentsProj = root.experiments().experimentID().createdAt().description();
-        Map<String, String> variablesNames = Map.of("projectID", "$projectID", "request", "$request");
-        Map<String, String> variablesTypes = Map.of("projectID", "ID!", "request", "ListExperimentRequest!");
+                        .build());
+        Map<String, String> variablesNames = Map.of(
+                "projectID", "$projectID",
+                "request", "$request");
+        Map<String, String> variablesTypes = Map.of(
+                "projectID", "ID!",
+                "request", "ListExperimentRequest!");
 
-        GraphQLQueryRequest graphQLQueryRequest = new GraphQLQueryRequest(listExperimentGraphQLQuery, experimentsProj);
+        ListExperimentGraphQLQuery listExperimentGraphQLQuery = ListExperimentGraphQLQuery
+                .newRequest()
+                .projectID((String)variables.get("projectID"))
+                .request((ListExperimentRequest)variables.get("request"))
+                .build();
+
+        GraphQLQueryRequest graphQLQueryRequest = new GraphQLQueryRequest(listExperimentGraphQLQuery, projection);
         GraphQLQueryRequestDecorator graphQLQueryRequestDecorator = new GraphQLQueryRequestDecorator(graphQLQueryRequest, variablesNames, variablesTypes);
 
         GraphQLRequest graphQLRequest = GraphQLRequest.builder()
                 .operationName(listExperimentGraphQLQuery.getOperationName())
                 .query(graphQLQueryRequestDecorator.serialize())
-                .variables(Map.of(
-                        "projectID",
-                        "ac3598af-0c62-4ad5-a323-848cafaaa881",
-                        "request",
-                        ListExperimentRequest
-                        .newBuilder()
-                        .pagination(Pagination
-                                .newBuilder()
-                                .page(0)
-                                .limit(7)
-                                .build())
-                        .build()))
+                .variables(variables)
                 .build();
 
         System.out.println(new ObjectMapper().writeValueAsString(graphQLRequest.getRequestBody()));
@@ -63,7 +144,8 @@ public class MyGraphQL {
         HttpHeaders headers = new HttpHeaders();
         headers.set("authorization", "");
         HttpEntity<?> request = new HttpEntity<>(graphQLRequest.getRequestBody(), headers);
-        String response = restTemplate.postForObject("http://localhost:8185/api/query", request, String.class);
+        JsonNode response = restTemplate.postForObject("http://localhost:8185/api/query", request, JsonNode.class);
         System.out.println(response);
+        System.out.println(new ObjectMapper().readValue(response.get("data").get("listExperiment").toString(), ListExperimentResponse.class));
     }
 }
